ec7d8493a4f6f2ea52751bb18c5589ce150cfb8d
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-02-15 20:58:51 +0000

    Add files via upload

---
+++ adding-calculator.js
@@ -0,0 +1,12 @@
+var firstNum = window.prompt("First number: ");
+
+number1 = parseInt(firstNum);
+
+var secondNum = window.prompt("Second number: ");
+
+number2 = parseInt(secondNum);
+
+
+alert("Press OK to see results.");
+
+alert(number1 + number2);
